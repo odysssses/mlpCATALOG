4e66fb0928e40386acd1eb4141d651305110b6f0
AverageN00bie <119633864+AverageN00bie@users.noreply.github.com>
2024-05-28 22:23:10 -0300

    Add files via upload

--- ZmlpCATALOGcolorful.py
+++ ZmlpCATALOGcolorful.py
@@ -1,7 +1,6 @@
 #HI! What this program does is it grabs a picture of a character from My Little Pony and makes an ASCII text of the character, colors it, and saves it. Cool right? :D.
 
-#WARNING! Works properly only on bash terminals.
-#HI! This project was made in python 3.10.12, so you're probably gonna need the interpreter for that. To execute it, you'll need to have all the pictures saved on the directory, and then just run it! If any errors occur, try messaging me or something, my discord is average_n00bie :D. By the way, the code saves the ASCII text in a file on the directory it's placed on. 
+#WARNING! Works properly only on bash terminals (because of colors).
 
 #----------------------------------------------------------------------------------------------------
 import PIL.Image
@@ -14,6 +13,7 @@ ASCIIchars = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
 
 nWidth = 100
 
+#compatibility!!!
 if system == "Windows":
     slash = "\\"
 else:
@@ -29,6 +29,16 @@ characters = [
     "Rarity",
     "Spike",
     "Twilight Sparkle",
+    "Flurry Heart",
+    "Thorax",
+    "Nightmare Moon",
+    "Queen Chrysalis",
+    "Lord Tirek",
+    "Cozy Glow",
+    "Grogar",
+    "Trixie",
+    "Discord",
+    "Sunset Shimmer",
 ]
 
 colors = [
@@ -44,16 +54,27 @@ colors = [
 ]
 
 #directories
+dirPath += slash
 charactersDir = [
-    dirPath + slash + "applejack.png",
-    dirPath + slash + "cadence.png",
-    dirPath + slash + "fluttershy.png",
-    dirPath + slash + "luna.png",
-    dirPath + slash + "pinkiePie.png",
-    dirPath + slash + "rainbowDash.png",
-    dirPath + slash + "rarity.png",
-    dirPath + slash + "spike.png",
-    dirPath + slash + "twilightSparkle.png",
+    dirPath + "applejack.png",
+    dirPath + "cadence.png",
+    dirPath + "fluttershy.png",
+    dirPath + "luna.png",
+    dirPath + "pinkiePie.png",
+    dirPath + "rainbowDash.png",
+    dirPath + "rarity.png",
+    dirPath + "spike.png",
+    dirPath + "sparkle.png",
+    dirPath + "heart.png",
+    dirPath + "thorax.png",
+    dirPath + "moon.png",
+    dirPath + "chrysalis.png",
+    dirPath + "tirek.png",
+    dirPath + "glow.png",
+    dirPath + "grogar.png",
+    dirPath + "trixie.png",
+    dirPath + "discord.png",
+    dirPath + "shimmer.png",
 ]
 
 #max width is 100
@@ -62,17 +83,18 @@ def resize_img(img):
     ratio = height/width/2
     nHeight = int(nWidth * ratio)
     resizedImg = img.resize((nWidth, nHeight))
-    return(resizedImg)
+    return resizedImg
 
+#the image needs to be grayscale cuz the text won't be like pixels
 def grayify(img):
     grayscaleImg = img.convert("L")
-    return(grayscaleImg)
+    return grayscaleImg
 
-#Trasformation O_o
+#trasformation O_o
 def pixelToASCII(img):
     pixels = img.getdata()
     chars = "".join([ASCIIchars[pixel//25] for pixel in pixels])
-    return(chars)    
+    return chars   
 
 while True:
     char = int(input(f"""Choose a character to show
@@ -85,6 +107,16 @@ while True:
     [6] {characters[6]}
     [7] {characters[7]}
     [8] {characters[8]}
+    [9] {characters[9]}
+    [10] {characters[10]}
+    [11] {characters[11]} 
+    [12] {characters[12]}
+    [13] {characters[13]}
+    [14] {characters[14]}
+    [15] {characters[15]}
+    [16] {characters[16]}
+    [17] {characters[17]}
+    [18] {characters[18]}
     : """))
     
     img = PIL.Image.open(charactersDir[char])
@@ -93,8 +125,11 @@ while True:
         
     pixelCount = len(nImgData)  
     asciiImg = "\n".join([nImgData[i:(i+nWidth)] for i in range(0, pixelCount, nWidth)])
-        
-    print(colors[char], asciiImg, "\033[49;2;0;0;0m")
+    
+    if char < 9 and system != "Windows":
+        print(colors[char], asciiImg, "\033[49;2;0;0;0m")
+    else:
+        print(asciiImg)
         
     with open(characters[char] +".txt", "w") as f:
         f.write(asciiImg)
